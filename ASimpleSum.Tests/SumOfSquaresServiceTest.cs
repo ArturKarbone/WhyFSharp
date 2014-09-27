a110eea49334ecacee406a9bb11efa9008cbb1f5
ArturKarbone <arturk@diatomenterprises.com>
2014-09-28 00:07:26 +0300

    -A SimpleSumService has been added
    -SumOfSquares method has been covered with tests

---
+++ ASimpleSum.Tests/SumOfSquaresServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASimpleSum.Tests
+{
+    [TestFixture]
+    class SumOfSquaresServiceTest
+    {
+        [Test]
+        public void SumOfSquares_SampleRange_CalulatesSum()
+        {
+            //Arrange
+            var service = new SumOfSquaresService();
+            var range = Enumerable.Range(1, 10);
+
+            //Act
+            var sum = service.SumOfSquares(range);
+
+            //Assert
+            Assert.AreEqual(385, sum);
+        }
+
+    }
+}
---
+++ ASimpleSum/SumOfSquaresService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASimpleSum
+{
+    public class SumOfSquaresService
+    {
+        public long SumOfSquares(IEnumerable<int> range)
+        {
+            long sum = 0;
+            foreach (int rangeElement in range)
+            {
+                sum += rangeElement * rangeElement;
+            }
+            return sum;
+        }
+    }
+}

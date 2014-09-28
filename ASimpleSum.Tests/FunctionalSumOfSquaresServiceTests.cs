1af43030242ad85897f94d949af545c171f5ec67
ArturKarbone <arturk@diatomenterprises.com>
2014-09-28 13:58:33 +0300

    FunctionalSumOfSquares + Tests have been added

---
+++ ASimpleSum.Tests/FunctionalSumOfSquaresServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASimpleSum.Tests
+{
+    [TestFixture]
+    class FunctionalSumOfSquaresServiceTests
+    {
+        [Test]
+        public void SumOfSquares_SampleRange_CalulatesSum()
+        {
+            //Arrange         
+            var service = new FunctionalSumOfSquaresService();
+            var range = Enumerable.Range(1, 10);
+
+            //Act
+            var sum = service.SumOfSquares(range);
+
+            //Assert
+            Assert.AreEqual(385, sum);
+        }
+
+
+        [Test]
+        public void SumOfSquares2_SampleRange_CalulatesSum()
+        {
+            //Arrange         
+            var service = new FunctionalSumOfSquaresService();
+            var range = Enumerable.Range(1, 10);
+
+            //Act
+            var sum = service.SumOfSquares2(range);
+
+            //Assert
+            Assert.AreEqual(385, sum);
+        }
+    }
+}
---
+++ ASimpleSum/FunctionalSumOfSquaresService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASimpleSum
+{
+    public class FunctionalSumOfSquaresService
+    {
+        public long SumOfSquares(IEnumerable<int> range)
+        {
+            return range
+                .Select(x => x * x)
+                .Sum();
+        }
+
+        public long SumOfSquares2(IEnumerable<int> range)
+        {
+            return range
+                .Sum(x => x * x);
+        }
+    }
+}
